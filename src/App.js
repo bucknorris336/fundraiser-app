80d614651aa09e823727ad3ee80c315d492c6a67
bucknorris336 <buckblocker90gmail.com>
2023-09-13 18:07:48 -0400

    added react router to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ItemPage from "./ItemPage"; // Import your ItemPage component
+import DonationsPage from "./DonationsPage"; // Import your DonationsPage component
 import { createTheme, ThemeProvider } from "@mui/material";
 import HomePage from "./HomePage";
 import Header from "./Header";
@@ -9,7 +12,13 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/item/:itemId" component={ItemPage} />
+          <Route path="/donations/:itemId" component={DonationsPage} />
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
